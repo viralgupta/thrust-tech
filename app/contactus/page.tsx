cf68b6cdb2c78a87162b8c5a8e607d9017a70256
viralgupta <guptaviral0@gmail.com>
2023-11-11 04:25:23 +0530

    connected sonner to fetch promise

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -14,6 +14,7 @@ const page = () => {
     email: "",
     message: "",
   });
+  const [submitting, setSubmitting] = useState(false)
 
   const formSchema = z.object({
     name: z.string().min(1, "Name field can not empty"),
@@ -34,31 +35,29 @@ const page = () => {
 
   const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setSubmitting(true);
     const validation = formSchema.safeParse(formData);
     if (validation.success) {
-      toast.loading("Submiting the form...")
       const formData = new FormData();
-      formData.append('Name', validation.data.name);
-      {validation.data.number && formData.append('Number', validation.data.number)}
-      formData.append('Email', validation.data.email);
-      formData.append('Message', validation.data.message);
-    
-      fetch("https://script.google.com/macros/s/AKfycbwWLoGo0h8g7qkW-X-TUhRzbs373smL5pW6PFv3F83qt1d2L4YTH6YEWHZ_ZQjUrj8B6A/exec", {
-        method: "POST",
-        body: formData,
-      })
-        .then(response => response.json())
-        .then(res => {
-          if (res.result == 'success') {
+      formData.append("Name", validation.data.name);
+      { validation.data.number && formData.append("Number", validation.data.number) }
+      formData.append("Email", validation.data.email);
+      formData.append("Message", validation.data.message);
+      const promise = fetch(
+        "https://script.google.com/macros/s/AKfycbwWLoGo0h8g7qkW-X-TUhRzbs373smL5pW6PFv3F83qt1d2L4YTH6YEWHZ_ZQjUrj8B6A/exec",
+        {
+          method: "POST",
+          body: formData,
+        }
+      )
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.result == "success") {
             setFormData({
               name: "",
               number: "",
               email: "",
               message: "",
-            });
-            toast.success("Your Message is delivered", {
-              description: "We will get back to you soon!",
-              duration: 2000,
             });
           } else {
             setFormData({
@@ -67,18 +66,17 @@ const page = () => {
               email: "",
               message: "",
             });
-            toast.error("There was an error submitting the form :(", {
-              description: "Please reach us through our socials!",
-              duration: 5000,
-            });
+            throw "error";
           }
+          setSubmitting(false);
         })
-        .catch(error => {
-          console.error("Error during fetch:", error);
-          toast.error("There was a network error while submitting the form :(", {
-            description: "Please try again later or reach us through our socials!",
-            duration: 5000,
-          });
+        toast.promise(promise, {
+          loading: 'Please Wait, Submitting the form...',
+          success: () => {
+            return `Your message has been delivered : )`;
+          },
+          error: 'Error Submitting Form, Please Reach Us Through Our Socials!',
+          duration: 5000
         });
     } else {
       let index = 0;
@@ -91,22 +89,23 @@ const page = () => {
         }
       };
       displayNextError();
+      setSubmitting(false);
     }
   };
 
   return (
     <>
-      <Toaster position="bottom-right" richColors closeButton duration={1000} />
       <section
         id="section-a"
         className="p-2 md:p-10 flex justify-center align-middle items-center pt-10"
         style={{ backgroundImage: `url(/img/section-f.webp)` }}
-      >
+        >
+        <Toaster position="bottom-right" richColors closeButton duration={1000} />
         <div className="relative mx-auto md:mx-0 md:translate-x-12 md:w-1/2 w-full text-center z-[1] md:px-12 block rounded-lg px-6 py-12 bg-[hsla(0,0%,100%,0.55)] shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.5)] dark:shadow-black/20 ">
           <h2 className="mb-12 text-xl md:text-3xl underline-offset-8 underline font-bold">
             Contact&nbsp;us
           </h2>
-          <form onSubmit={e => submitForm(e)}>
+          <form onSubmit={(e) => submitForm(e)}>
             <div className="relative mb-6" data-te-input-wrapper-init>
               <input
                 type="text"
@@ -175,21 +174,26 @@ const page = () => {
               type="submit"
               data-te-ripple-init
               data-te-ripple-color="light"
+              disabled={submitting}
               onClick={(e) => {
                 e.preventDefault();
                 submitForm(e as unknown as React.FormEvent<HTMLFormElement>);
               }}
-              className="inline-block w-full rounded border bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0"
+              className="inline-block disabled:cursor-not-allowed w-full rounded border bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] lg:mb-0"
             >
               Send
             </button>
-            
           </form>
         </div>
         <div className="relative hidden md:block text-end">
-        <a href='https://www.google.com/maps/place/Thrust+Tech/@28.6775365,77.3611831,10.32z/data=!4m6!3m5!1s0x390cf18883097b0f:0x14d61ddbe213e804!8m2!3d28.6689899!4d77.4502252!16s%2Fg%2F11ss3clxpg?entry=ttu' target="_blank" rel="noopener" className="absolute bottom-0 right-0 m-2 z-10 p-1 bg-red-700 text-xs text-white w-min rounded-sm h-min align-bottom hover:bg-gray-900 duration-300 mt-1 ml-auto lowercase cursor-pointer">
+          <a
+            href="https://www.google.com/maps/place/Thrust+Tech/@28.6775365,77.3611831,10.32z/data=!4m6!3m5!1s0x390cf18883097b0f:0x14d61ddbe213e804!8m2!3d28.6689899!4d77.4502252!16s%2Fg%2F11ss3clxpg?entry=ttu"
+            target="_blank"
+            rel="noopener"
+            className="absolute bottom-0 right-0 m-2 z-10 p-1 bg-red-700 text-xs text-white w-min rounded-sm h-min align-bottom hover:bg-gray-900 duration-300 mt-1 ml-auto lowercase cursor-pointer"
+          >
             View&nbsp;In&nbsp;Maps
-        </a>
+          </a>
           <img
             src="/img/maps.png"
             alt="maps"
