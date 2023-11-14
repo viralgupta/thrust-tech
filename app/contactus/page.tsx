c64331b9bc0a7288f687d3ebe64da6a213eaaaf8
viralgupta <guptaviral0@gmail.com>
2023-11-14 16:49:54 +0530

    fixed a tag loads before in contact us

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
     email: "",
     message: "",
   });
-  const [submitting, setSubmitting] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
 
   const formSchema = z.object({
     name: z.string().min(1, "Name field can not empty"),
@@ -40,7 +40,10 @@ const page = () => {
     if (validation.success) {
       const formData = new FormData();
       formData.append("Name", validation.data.name);
-      { validation.data.number && formData.append("Number", validation.data.number) }
+      {
+        validation.data.number &&
+          formData.append("Number", validation.data.number);
+      }
       formData.append("Email", validation.data.email);
       formData.append("Message", validation.data.message);
       const promise = fetch(
@@ -69,15 +72,15 @@ const page = () => {
             throw "error";
           }
           setSubmitting(false);
-        })
-        toast.promise(promise, {
-          loading: 'Please Wait, Submitting the form...',
-          success: () => {
-            return `Your message has been delivered : )`;
-          },
-          error: 'Error Submitting Form, Please Reach Us Through Our Socials!',
-          duration: 5000
         });
+      toast.promise(promise, {
+        loading: "Please Wait, Submitting the form...",
+        success: () => {
+          return `Your message has been delivered : )`;
+        },
+        error: "Error Submitting Form, Please Reach Us Through Our Socials!",
+        duration: 5000,
+      });
     } else {
       let index = 0;
       const displayNextError = () => {
@@ -93,14 +96,24 @@ const page = () => {
     }
   };
 
+  function handleImageLoad() {
+    document.getElementById('parentDiv')!.style.display = 'block';
+    // document.querySelector('#parentDiv a')!.style.display = 'inline-block';
+  }
+
   return (
     <>
       <section
         id="section-a"
         className="p-2 md:p-10 flex justify-center align-middle items-center pt-10"
         style={{ backgroundImage: `url(/img/section-f.webp)` }}
-        >
-        <Toaster position="bottom-right" richColors closeButton duration={1000} />
+      >
+        <Toaster
+          position="bottom-right"
+          richColors
+          closeButton
+          duration={1000}
+        />
         <div className="relative mx-auto md:mx-0 md:translate-x-12 md:w-1/2 w-full text-center z-[1] md:px-12 block rounded-lg px-6 py-12 bg-[hsla(0,0%,100%,0.55)] shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] backdrop-blur-[30px] dark:bg-[hsla(0,0%,5%,0.5)] dark:shadow-black/20 ">
           <h2 className="mb-12 text-xl md:text-3xl underline-offset-8 underline font-bold">
             Contact&nbsp;us
@@ -185,19 +198,22 @@ const page = () => {
             </button>
           </form>
         </div>
-        <div className="relative hidden md:block text-end">
+        <div id="parentDiv" className="relative hidden md:block text-end">
           <a
             href="https://www.google.com/maps/place/Thrust+Tech/@28.6775365,77.3611831,10.32z/data=!4m6!3m5!1s0x390cf18883097b0f:0x14d61ddbe213e804!8m2!3d28.6689899!4d77.4502252!16s%2Fg%2F11ss3clxpg?entry=ttu"
             target="_blank"
             rel="noopener"
             className="absolute bottom-0 right-0 m-2 z-10 p-1 bg-red-700 text-xs text-white w-min rounded-sm h-min align-bottom hover:bg-gray-900 duration-300 mt-1 ml-auto lowercase cursor-pointer"
+            style={{display: "none"}}
           >
-            View&nbsp;In&nbsp;Maps
+            View In Maps
           </a>
           <img
             src="/img/maps.png"
             alt="maps"
             className="rounded-lg opacity-90 hover:opacity-100 duration-500"
+            onLoad={handleImageLoad}
+            style={{display: "none"}}
           />
         </div>
       </section>
